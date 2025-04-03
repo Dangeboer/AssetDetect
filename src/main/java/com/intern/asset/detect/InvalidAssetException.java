198106bb3589834d0716297fada10b8f1dcdce34
CYX <dangeboe@gmail.com>
2025-04-02 23:58:56 -0400

    Modify Structure

--- src/main/java/com/intern/asset/detect/InvalidAssetException.java
+++
@@ -1,7 +0,0 @@
-package com.intern.asset.function.detect;
-
-public class InvalidAssetException extends RuntimeException {
-    public InvalidAssetException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/intern/asset/detect/PingException.java
+++
@@ -1,7 +0,0 @@
-package com.intern.asset.function.ping;
-
-public class PingException extends RuntimeException {
-    public PingException(String message) {
-        super(message);
-    }
-}
